6a72eb7697cd556d52c6c1fa26b6b3a7a0d16b6a
sanjana.anantbhat178@gmail.com <sanjana.anantbhat178@gmail.com>
2019-11-04 12:47:33 +0100

    Done with the stupid coach

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  # get 'questions/ask'
+  # get 'questions/answer'
+  get 'ask', to: 'questions#ask'
+  get 'answer', to: 'questions#answer'
+
+  # get 'about', to: 'pages#about', as: :about
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
